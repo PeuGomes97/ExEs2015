34067794288d1e84701a8a3ecd44f39761d2e807
PeuGomes97 <peugomes97@gmail.com>
2023-08-09 16:25:28 -0300

    Let and Var exercise

---
+++ letandvar.js
@@ -0,0 +1,18 @@
+// - What is the difference between ***var*** and ***let***?
+
+ANSWER - 'Let works throught the block scope, while var can be accessed by a hoisted variable. Var can be redeclared and reassigned. Let cant redeclare'
+
+
+// - What is the difference between **var** and ***const***?
+
+ANSWER - 'You can reassign and redeclare with var, but you can not redeclare or reassign using the const keyword.'
+
+// - What is the difference between **let** and **const**?
+
+ANSWER - 'You can reassign with let. Const cant reassing and redeclare'
+
+// - What is hoisting?
+
+
+ANSWER - 'It’s the way that we explain how the JS compiler works. Variables are lifting or “hoisted” to the top of the scope they are declared in. When using ***var***, you can access the variable name and it’s undefined value before it is used later on'
+
